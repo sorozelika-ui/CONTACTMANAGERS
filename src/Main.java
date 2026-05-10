7f0c09cae874e614a2d9fbfef527223c7b1bd44a
sorowagniguizelika <sorozelika@gmail.com>
2026-05-10 10:30:44 +0000

    cration de la variable contact et ses attributs

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,16 @@ public class Main {
     public static void main(String[] args) {
 
         ContactsManager myContactsManager = new ContactsManager();
+
+        // attributs de la variable contact
+        String[] name = {"zélika", "aicha", "haramatou", "zeïnab"};
+
+        String[] phoneNumber = {
+                "0788475809",
+                "0102254627",
+                "0788350610",
+                "0707739576"
+        };
+
     }
 }
