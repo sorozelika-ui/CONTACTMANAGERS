0cb88ee8f141b0805ae4b99657a1f8254ff3257a
sorowagniguizelika <sorozelika@gmail.com>
2026-05-10 10:33:50 +0000

    appel de la methode d'ajout

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,2 +1,13 @@
 public class ContactsManager {
+    Contact[] myFriends;
+    int friendsCount;
+    ContactsManager() {
+        this.friendsCount = 0;
+        this.myFriends = new Contact[500];
+    }
+
+    //methode d'ajout de contact
+    void addContact(Contact contact) {
+        myFriends[friendsCount] = contact;
+        friendsCount++;}
 }
